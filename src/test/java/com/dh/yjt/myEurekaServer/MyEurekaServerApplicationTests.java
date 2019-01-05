20226d73972ffffd5064718bbb7bf4786470fb68
yuejuntao <254647202@qq.com>
2019-01-05 12:40:54 +0800

    创建EurekaServer

---
+++ src/test/java/com/dh/yjt/myEurekaServer/MyEurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dh.yjt.myEurekaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
